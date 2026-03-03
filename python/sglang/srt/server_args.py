797d6dd34969055f855ad2d750bc8457ef480450
Shurui Jia <sr@ShuruideMacBook-Pro-2.local>
2026-03-03 16:35:38 +0800

    For CP: support cp4 in round-robin-split mode

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -1306,9 +1306,6 @@ class ServerArgs:
                         else:
                             self.enable_dp_attention = True
                             self.moe_dense_tp_size = 1
-                            assert (
-                                self.dp_size == 1
-                            ), "For round-robin split mode, dp attention is not supported."
                         assert (
                             self.tp_size == 8
                         ), "Current multi-machine CP support suffers from precision issues. So context parallel only support Single machine(tp_size == 8)"
